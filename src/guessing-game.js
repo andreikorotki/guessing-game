e4b5062c281b497d1f88dd72f571e97851233b51
Andrei Karotki <Andreikorotki@gmail.com>
2021-08-24 18:00:00 +0300

    feat: add guessing_game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.solutionCandidate = 0;
+        this.divideAssumptions = 2;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max; 
     }
 
     guess() {
-
+        this.solutionCandidate = Math.floor((this.min + this.max + 1) / this.divideAssumptions );
+        return this.solutionCandidate;
     }
 
     lower() {
-
+        this.max = this.solutionCandidate;
     }
 
     greater() {
-
+        this.min = this.solutionCandidate;
     }
 }
 
